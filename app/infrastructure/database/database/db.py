b4df8ac4b5dfd211268b9b9205d0da7d0c10956e
zobkov <112057887+zobkov@users.noreply.github.com>
2025-11-20 22:44:33 +0300

    feat: add feedback db model

--- app/infrastructure/database/database/db.py
+++ app/infrastructure/database/database/db.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.infrastructure.database.database.users import _UsersDB
+from app.infrastructure.database.database.feedback import _FeedbackDB
 from app.infrastructure.database.database.quiz_dod import _QuizDodDB
 from app.infrastructure.database.database.quiz_dod_users_info import _QuizDodUsersInfoDB
 
@@ -9,6 +10,7 @@ class DB:
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
         self.users = _UsersDB(session=session)
+        self.feedback = _FeedbackDB(session=session)
         self.quiz_dod = _QuizDodDB(session=session)
         self.quiz_dod_users_info = _QuizDodUsersInfoDB(session=session)
 
