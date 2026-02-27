8d2042afb7638bcd5c2a572985abe289eb5cdc09
Artem <zobkovartyom@gmail.com>
2026-02-27 21:52:31 +0300

    fix start function bug

--- app/bot/dialogs/selections/creative/dialogs.py
+++ app/bot/dialogs/selections/creative/dialogs.py
@@ -46,6 +46,7 @@ from .handlers import (
     on_university_entered,
 )
 from .states import CreativeSelectionSG
+from app.bot.dialogs.guest.states import GuestMenuSG
 
 _SUCCESS_TEXT = (
     "🎉 <b>Заявка успешно отправлена!</b>\n\n"
@@ -89,7 +90,7 @@ creative_selection_dialog = Dialog(
         Format("{intro_text}"),
         Column(
             Button(Const("📝 Подать заявку"), id="start_application", on_click=on_start_clicked),
-            Start(Const("🏠 Назад"), id="cancel_main", mode=StartMode.RESET_STACK),
+            Start(Const("🏠 Назад"), id="cancel_main", mode=StartMode.RESET_STACK, state=GuestMenuSG.MAIN),
         ),
         state=CreativeSelectionSG.MAIN,
         getter=[get_main_text, get_creative_intro_media],
